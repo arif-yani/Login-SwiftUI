e02dc1910be8f325bdd64f1207d74671558f7921
arif-yani <muhamadarifyani@gmail.com>
2022-07-12 16:13:55 +0700

    Initial Commit

---
+++ Login-SwiftUI/Login_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_SwiftUIApp.swift
+//  Login-SwiftUI
+//
+//  Created by Muhamad Arif on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Login_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
